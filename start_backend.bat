b823270a46d7c6f8eef0401d5c692cd5272ccfa7
Raj-singh73 <rajsingh700777@gmail.com>
2026-09-04 22:44:10 +0530

    chore: update start_backend.bat

--- start_backend.bat
+++ start_backend.bat
@@ -3,10 +3,5 @@ echo ======================================================================
 echo           Starting WeatherGPT FastAPI Backend Server (SIH 2026)
 echo ======================================================================
 cd /d "%~dp0"
-python backend\main.py
-if errorlevel 1 (
-    if exist "C:\Users\rajs6\anaconda3\python.exe" (
-        "C:\Users\rajs6\anaconda3\python.exe" backend\main.py
-    )
-)
+C:\Users\rajs6\anaconda3\python.exe backend\main.py
 pause
